1a1def792e7e66d0593d437015a5a7b63159a4d5
Nolan Neeley <nolanneeley@gmail.com>
2018-08-13 08:59:30 -0500

    how to kill the rails server in use

--- notes.rb
+++ notes.rb
@@ -4,3 +4,11 @@
 # git pull origin master
 # git merge test
 # git push origin master
+
+# How to kill rails server if already in use:
+# https://stackoverflow.com/questions/31039998/address-already-in-use-bind2-errnoeaddrinuse
+#   1. run this to find out what process is running on the port 3000:
+#     lsof -wni tcp:3000
+#
+#   2. now kill the process by using the PID that comes up, example:
+#     kill -9 3366
