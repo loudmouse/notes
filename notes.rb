459f0d9c37c42c75694a6a1ffd3d2e4208020262
Nolan Neeley <nolanneeley@gmail.com>
2018-08-12 20:45:48 -0500

    notes for merging branch into master

---
+++ notes.rb
@@ -0,0 +1,6 @@
+# Merge local branch into master
+# https://stackoverflow.com/questions/5601931/best-and-safest-way-to-merge-a-git-branch-into-master
+# git checkout master
+# git pull origin master
+# git merge test
+# git push origin master
